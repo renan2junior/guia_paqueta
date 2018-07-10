c83b6c1930de365e2c9420450adb9cf1f75e264b
Renan Junior <renan2junior@gmail.com>
2018-07-10 16:21:49 -0300

    Inicio.

---
+++ guia_paqueta/guia_paqueta/guia_paqueta/guia_paqueta.Android/obj/Debug/android/src/md57094d3fa731cc2a0b303f3bbdf3c571a/MainActivity.java
@@ -0,0 +1,47 @@
+package md57094d3fa731cc2a0b303f3bbdf3c571a;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("guia_paqueta.Droid.MainActivity, guia_paqueta.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("guia_paqueta.Droid.MainActivity, guia_paqueta.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
